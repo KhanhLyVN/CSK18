211ad36782c5642fe34888db7422994b148d89e6
nhims_09 <134600549+GiaLinhcutis1tg@users.noreply.github.com>
2026-08-11 17:49:38 +0700

    k bt

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDiGtSXQMGEE1YWWfW73enu6PK3zjokrw4",
-  authDomain: "faq-csk18.firebaseapp.com",
-  projectId: "faq-csk18",
-  storageBucket: "faq-csk18.firebasestorage.app",
-  messagingSenderId: "556457828953",
-  appId: "1:556457828953:web:da7973f61f0ac8e5408748",
-  measurementId: "G-X0GM41LVMJ"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDiGtSXQMGEE1YWWfW73enu6PK3zjokrw4",
+    authDomain: "faq-csk18.firebaseapp.com",
+    projectId: "faq-csk18",
+    storageBucket: "faq-csk18.firebasestorage.app",
+    messagingSenderId: "556457828953",
+    appId: "1:556457828953:web:da7973f61f0ac8e5408748",
+    measurementId: "G-X0GM41LVMJ"
+  };
 
-firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+  const storage = firebase.storage();
